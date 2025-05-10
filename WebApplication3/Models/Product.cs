8dcde491880a7f0a18f17aaf9df235ad40bbf077
roryTheGnome <s29991@pjwstk.edu.pl>
2025-05-10 16:56:21 +0200

    needed tables added as classes

---
+++ WebApplication3/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+}// just realized i dont need rest of the tables as classes
---
+++ WebApplication3/Models/Product_Warehouse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Models;
+
+public class Product_Warehouse
+{
+    public int IdProduct { get; set; }
+    public int IdWarehouse { get; set; }
+    public int Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
